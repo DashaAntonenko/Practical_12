39d3b976a53d4af7f00a54f413051573c4be6138
Даша Антоненко <dasha.antonenko.04@mail.ru>
2024-04-06 00:17:41 +0300

    Practical

--- src/com/mirea/kt/example/Practical.java
+++ src/com/mirea/kt/example/Practical.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Practical {
 
     public static void main(String[] args) {
+        System.out.println("Антоненко Дарья, рибо-04-22, вариант 2");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите путь к файлу: ");
         String path = scanner.nextLine();
